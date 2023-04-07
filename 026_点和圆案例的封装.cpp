9cbd1db1f4a29f4c215b24ff9f31ff78657e4501
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-04-07 17:01:34 +0800

    Add files via upload

---
+++ 026_点和圆案例的封装.cpp
@@ -0,0 +1,20 @@
+﻿#include"iostream"
+#include "mycircle.h"
+#include "mypoint.h"
+using namespace std;
+void main()
+{
+	mycircle c1;
+	mypoint p1;
+	c1.setmycircle(2, 3, 3);
+	p1.setpoint(2, 2);
+	int ret = c1.judge(p1);
+	if (ret == 1)
+	{
+		printf("p点在圆内");
+	}
+	else
+	{
+		printf("p点不在圆内");
+	}
+}
