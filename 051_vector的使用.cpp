15d24d1d92ceb8832915b36155ddb7d11adf06d1
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-05-05 10:37:25 +0800

    Add files via upload

---
+++ 051_vector的使用.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+void main()
+{
+	vector<int>a;
+	a.push_back(2);
+	a.push_back(1);
+	a.push_back(3);
+	a.pop_back();
+	cout << a[0] << ' ' << a[1] << endl;
+	cout << "hello..." << endl;
+	system("pause");
+}
